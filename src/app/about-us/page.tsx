8fcb0ff362089873f71d52c7f8b368cbe96ca378
ShauaktChaboi <ali.chaboi007@gmail.com>
2024-11-13 02:10:29 +0500

    add:pages are added home,our-team and about-us

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+export default function AboutUs() {
+  return (
+    <footer className="">
+      <div className=" text-black">
+        <p> About us </p>
+      </div>
+    </footer>
+  );
+}
---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+export default function Footer() {
+  return (
+    <footer className="">
+      <div className=" text-black">
+        <p> Home </p>
+      </div>
+    </footer>
+  );
+}
